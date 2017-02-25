9f1f142e4908f4a87dd13c838bdea90a7b3d65a1
MathiasAntunes <mathias.antunes@etu.univ-poitiers.fr>
2017-02-25 19:30:31 +0100

    wtf erreur ligne 26 dans truc2

--- truc.php
+++ truc.php
@@ -3,6 +3,8 @@
 	<head>
 		<meta charset="UTF-8"/>
 			<title>Test question</title>
+		<link href="//cdn.muicss.com/mui-0.9.9/css/mui.min.css" rel="stylesheet" type="text/css" />
+		<script src="//cdn.muicss.com/mui-0.9.9/js/mui.min.js"></script>
 	</head>
 					 <!--faire un deuxième fichier dans celui là juste laisser la décla de tabRep dans l'autre faire les conditions et le remplissage de tab rep-->
 	<body>
@@ -12,7 +14,7 @@
 		?>
 		Pret à commencer ?
 		<form id="formulaire" action="truc2.php" method = "post">
-			<input class="btn type="submit" value="C'est parti !" name = "Go"/>
+			<input class="mui-btn mui-btn--primary" type="submit" value="C'est parti !" name = "Go"/>
 		</form>
 	</body>
 </html>
